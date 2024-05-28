852e81230d54ffb1aff16e470e38376ce7bcb011
Muhammad Umer Nasir <67263090+UmerNasir-2000@users.noreply.github.com>
2024-05-28 13:56:05 +0500

    feat: add react hook form and zod

--- src/components/contactForm/index.tsx
+++ src/components/contactForm/index.tsx
@@ -1,6 +1,20 @@
+import { useCallback } from "react";
+import { useForm } from "react-hook-form";
+
 const ContactForm = () => {
+  const { register, handleSubmit, reset } = useForm();
+
+  const onSubmit = useCallback((data: any) => {
+    console.log("data :>> ", data);
+    console.log(`Submitted..`);
+    reset();
+  }, []);
+
   return (
-    <form className="bg-white p-6 px-8 rounded-xl shadow-md w-1/3 space-y-6">
+    <form
+      className="bg-white p-6 px-8 rounded-xl shadow-md w-1/3 space-y-6"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <h2 className="text-3xl font-bold text-grey-900">Contact Us</h2>
       <section className="space-y-3">
         <div className="flex gap-3 my-3">
@@ -13,6 +27,7 @@ const ContactForm = () => {
               <span className="text-green-700 font-bold ml-1">*</span>
             </label>
             <input
+              {...register("firstName", { required: true })}
               type="text"
               id="firstName"
               required
@@ -27,6 +42,7 @@ const ContactForm = () => {
               Last Name <span className="text-green-700 font-bold ml-1">*</span>
             </label>
             <input
+              {...register("lastName", { required: true })}
               type="text"
               id="lastName"
               required
@@ -41,6 +57,7 @@ const ContactForm = () => {
               <span className="text-green-700 font-bold ml-1">*</span>
             </label>
             <input
+              {...register("email", { required: true })}
               type="email"
               id="email"
               required
@@ -55,6 +72,7 @@ const ContactForm = () => {
           </label>
           <textarea
             id="message"
+            {...register("message", { required: true })}
             required
             className="px-4 p-2 w-full rounded-md border border-grey-500 focus:border-green-700 focus:outline-none"
             rows={3}
@@ -62,7 +80,12 @@ const ContactForm = () => {
         </div>
 
         <div className="flex items-center gap-x-3">
-          <input id="consent" type="checkbox" required />
+          <input
+            id="consent"
+            type="checkbox"
+            required
+            {...register("consent", { required: true })}
+          />
           <label htmlFor="consent" className="text-grey-900 text-sm">
             I consent to being contacted by the team
             <span className="text-green-700 ml-1 font-bold">*</span>
