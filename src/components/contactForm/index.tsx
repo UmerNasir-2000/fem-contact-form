460a06275e91dea945fa5fd94388cc8fd0a6fc31
Muhammad Umer Nasir <67263090+UmerNasir-2000@users.noreply.github.com>
2024-05-28 13:28:10 +0500

    feat: add textarea for message

--- src/components/contactForm/index.tsx
+++ src/components/contactForm/index.tsx
@@ -2,48 +2,65 @@ const ContactForm = () => {
   return (
     <form className="bg-white p-6 px-8 rounded-xl shadow-md w-1/3">
       <h2 className="text-3xl font-bold text-grey-900">Contact Us</h2>
-      <div className="flex gap-3 my-3">
-        <div className="space-y-2 w-1/2">
-          <label
-            htmlFor="firstName"
-            className="block font-medium text-gray-700"
-          >
-            First Name <span className="text-green-700">*</span>
+      <section className="space-y-3">
+        <div className="flex gap-3 my-3">
+          <div className="space-y-2 w-1/2">
+            <label
+              htmlFor="firstName"
+              className="block font-medium text-gray-700"
+            >
+              First Name <span className="text-green-700">*</span>
+            </label>
+            <input
+              type="text"
+              id="firstName"
+              className="w-full p-2 rounded-md border border-grey-500 focus:border-green-700 focus:outline-none"
+            />
+          </div>
+          <div className="space-y-2 w-1/2">
+            <label
+              htmlFor="lastName"
+              className="block font-medium text-gray-700"
+            >
+              Last Name <span className="text-green-700">*</span>
+            </label>
+            <input
+              type="text"
+              id="lastName"
+              className="p-2 w-full rounded-md border border-grey-500 focus:border-green-700 focus:outline-none"
+            />
+          </div>
+        </div>
+        <div>
+          <div className="space-y-2 w-full">
+            <label htmlFor="email" className="block font-medium text-gray-700">
+              Email Address <span className="text-green-700">*</span>
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="p-2 w-full rounded-md border border-grey-500 focus:border-green-700 focus:outline-none"
+            />
+          </div>
+        </div>
+
+        <div className="space-y-2">
+          <label htmlFor="message" className="block font-medium text-gray-700">
+            Message <span className="text-green-700">*</span>
           </label>
-          <input
-            type="text"
-            id="firstName"
-            className="w-full p-2 rounded-md border border-green-700 focus:border-green-700 focus:outline-none"
+          <textarea
+            id="message"
+            className="p-2 w-full rounded-md border border-grey-500 focus:border-green-700 focus:outline-none"
+            rows={3}
           />
         </div>
-        <div className="space-y-2 w-1/2">
-          <label htmlFor="lastName" className="block font-medium text-gray-700">
-            Last Name <span className="text-green-700">*</span>
-          </label>
-          <input
-            type="text"
-            id="lastName"
-            className="p-2 w-full rounded-md border border-green-700 focus:border-green-700 focus:outline-none"
-          />
+
+        <div className="mt-3">
+          <button className="bg-green-600 text-white w-full px-2 py-3 rounded-md transition-opacity hover:bg-green-600/70">
+            Submit
+          </button>
         </div>
-      </div>
-      <div className="">
-        <div className="space-y-2 w-full">
-          <label htmlFor="email" className="block font-medium text-gray-700">
-            Email Address <span className="text-green-700">*</span>
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="p-2 w-full rounded-md border border-green-700 focus:border-green-700 focus:outline-none"
-          />
-        </div>
-      </div>
-      <div className="mt-3">
-        <button className="bg-green-600 text-white w-full px-2 py-3 rounded-md transition-opacity hover:bg-green-600/70">
-          Submit
-        </button>
-      </div>
+      </section>
     </form>
   );
 };
