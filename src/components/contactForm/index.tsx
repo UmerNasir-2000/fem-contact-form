974085e463517e4ef620bf857805cb18d6ee1264
Muhammad Umer Nasir <67263090+UmerNasir-2000@users.noreply.github.com>
2024-05-28 13:34:45 +0500

    feat: add basic checkbox

--- src/components/contactForm/index.tsx
+++ src/components/contactForm/index.tsx
@@ -1,13 +1,13 @@
 const ContactForm = () => {
   return (
-    <form className="bg-white p-6 px-8 rounded-xl shadow-md w-1/3">
+    <form className="bg-white p-6 px-8 rounded-xl shadow-md w-1/3 space-y-6">
       <h2 className="text-3xl font-bold text-grey-900">Contact Us</h2>
       <section className="space-y-3">
         <div className="flex gap-3 my-3">
           <div className="space-y-2 w-1/2">
             <label
               htmlFor="firstName"
-              className="block font-medium text-gray-700"
+              className="block font-medium text-gray-900"
             >
               First Name <span className="text-green-700">*</span>
             </label>
@@ -20,7 +20,7 @@ const ContactForm = () => {
           <div className="space-y-2 w-1/2">
             <label
               htmlFor="lastName"
-              className="block font-medium text-gray-700"
+              className="block font-medium text-gray-900"
             >
               Last Name <span className="text-green-700">*</span>
             </label>
@@ -33,7 +33,7 @@ const ContactForm = () => {
         </div>
         <div>
           <div className="space-y-2 w-full">
-            <label htmlFor="email" className="block font-medium text-gray-700">
+            <label htmlFor="email" className="block font-medium text-gray-900">
               Email Address <span className="text-green-700">*</span>
             </label>
             <input
@@ -45,7 +45,7 @@ const ContactForm = () => {
         </div>
 
         <div className="space-y-2">
-          <label htmlFor="message" className="block font-medium text-gray-700">
+          <label htmlFor="message" className="block font-medium text-gray-900">
             Message <span className="text-green-700">*</span>
           </label>
           <textarea
@@ -53,6 +53,14 @@ const ContactForm = () => {
             className="p-2 w-full rounded-md border border-grey-500 focus:border-green-700 focus:outline-none"
             rows={3}
           />
+        </div>
+
+        <div className="flex items-center gap-x-3">
+          <input id="consent" type="checkbox" />
+          <label htmlFor="consent" className="text-grey-900 text-sm">
+            I consent to being contacted by the team{" "}
+            <span className="text-green-700">*</span>
+          </label>
         </div>
 
         <div className="mt-3">
